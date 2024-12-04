80eabd7af91df5a713aaec2f3b41af1382e57063
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2024-12-04 12:20:28 +0500

    build(deps): update page.tsx, input.tsx, package-lock.json and package.json jmjnkj

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { AlertDialogDemo } from "./Components/alert-dialog";
 import { BadgeDemo } from "./Components/badge";
 import { BreadcrumbDemo } from "./Components/BreadCrumb";
 import { DatePickerDemo } from "./Components/Calendar";
+import { CardWithForm } from "./Components/card";
 import { DataTableDemo } from "./Components/DataTable";
 
 export default function Home() {
   return (
     <div className="w-full">
       <div className="w-full mt-2 flex flex-col justify-start items-start gap-2">
+        <CardWithForm />
         <DataTableDemo />
         <AccordionDemo />
         <AlertDestructive />
