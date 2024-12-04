f477cb478f9a27904aece8f5c37313f4798ad117
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2024-12-04 14:05:12 +0500

    update 5 files fdsfdfsdf

--- app/Components/card.tsx
+++ app/Components/card.tsx
@@ -21,7 +21,7 @@ import {
 
 export function CardWithForm() {
   return (
-    <Card className="w-[350px]">
+    <Card className="w-full">
       <CardHeader>
         <CardTitle>Create project</CardTitle>
         <CardDescription>Deploy your new project in one-click.</CardDescription>
--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,14 @@ import { BadgeDemo } from "./Components/badge";
 import { BreadcrumbDemo } from "./Components/BreadCrumb";
 import { DatePickerDemo } from "./Components/Calendar";
 import { CardWithForm } from "./Components/card";
+import { ChartDemo } from "./Components/Chart";
 import { DataTableDemo } from "./Components/DataTable";
 
 export default function Home() {
   return (
     <div className="w-full">
       <div className="w-full mt-2 flex flex-col justify-start items-start gap-2">
+        <ChartDemo/>
         <CardWithForm />
         <DataTableDemo />
         <AccordionDemo />
