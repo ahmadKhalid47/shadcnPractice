8cb89410e9c2be5e99d900f77793ac30f52b358f
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-07 22:52:48 +0500

    bvksdbvksdbh

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
 "use client";
-import { AccordionDemo } from "./Components/accordian";
-import { AlertDestructive } from "./Components/alert";
-import { AlertDialogDemo } from "./Components/alert-dialog";
-import { BadgeDemo } from "./Components/badge";
-import { BreadcrumbDemo } from "./Components/BreadCrumb";
-import { DatePickerDemo } from "./Components/Calendar";
-import { CardWithForm } from "./Components/card";
-import { ChartDemo } from "./Components/Chart";
-import { DataTableDemo } from "./Components/DataTable";
-import { DialogDemo } from "./Components/dialog";
 
 export default function Home() {
   return (
     <div className="w-full">
       <div className="w-full mt-2 flex flex-col justify-start items-start gap-2">
-        <DialogDemo />
-        <ChartDemo />
-        <CardWithForm />
-        <DataTableDemo />
-        <AccordionDemo />
-        <AlertDestructive />
-        <AlertDialogDemo />
-        <BadgeDemo />
-        <BreadcrumbDemo />
-        <DatePickerDemo />
       </div>
     </div>
   );
