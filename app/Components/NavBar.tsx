10ac1f97122376668cb97fd5a10e80515d34eb3f
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-11 01:28:51 +0500

    update NavBar.tsx and sidebar.tsx refs

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -1,11 +1,33 @@
+import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Github, Linkedin } from "lucide-react";
 
 export function Navbar() {
   return (
-                <div className="w-full h-fit sticky top-2 flex justify-between items-center">
-                  <SidebarTrigger className="" />
-                  <SidebarTrigger className="" />
-                </div>
-
+    <div className="w-full h-fit sticky top-2 flex justify-between items-center">
+      <SidebarTrigger />
+      <div className="flex justify-start items-center gap-3">
+        <a
+          href="https://drive.google.com/file/d/1yMEWbeH8t8gUJfA2SD9V0CaRXLVAjfzg/view?usp=drive_link"
+          target="_blank"
+        >
+          <Button>Resume</Button>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ahmadraza47/"
+          target="_blank"
+          className="hover:text-white"
+        >
+          <Linkedin />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ahmadraza47/"
+          target="_blank"
+          className="hover:text-white"
+        >
+          <Github />
+        </a>
+      </div>
+    </div>
   );
 }
