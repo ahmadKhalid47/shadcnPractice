5caf2bcbf5fbf115431c014974ced8bcd2b66578
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-12 22:15:03 +0500

    update 8 files bhjb

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,35 @@
 "use client";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import profilePic from "../public/IMG-20241208-WA0041.jpg";
 export default function Home() {
   return (
     <>
       <div className="w-full">
-        <h1 className="text-5xl text-cente ">A Brief About Me</h1>
-        <div className="bg-red-40 w-full xl:w-[700px]">
-          <p className="text-lg text-center md:text-justify pt-4">
-            I am a passionate MERN stack developer with one year of hands-on
-            experience building and deploying dynamic web applications. Having
-            worked on various MERN stack projects, I have gained valuable skills
-            in creating user-friendly and efficient solutions. Currently, I am
-            in my 5th semester of studies, constantly enhancing my knowledge and
-            expertise in web development to deliver innovative and reliable
-            applications.{" "}
-          </p>
+        <h1 className="text-5xl text-center text-gray-400 mt-3 lg:mt-10">
+          About
+        </h1>
+        <div className="bg-red-40 w-full flex lg:flex-row flex-col justify-center lg:justify-start items-center mt-8 lg:mt-24 gap-5">
+          <Avatar className="w-52 h-52 rounded-md">
+            <AvatarImage
+              className="object-cover rounded-3xl"
+              src={profilePic.src}
+            />
+            <AvatarFallback className="rounded-3xl">Profile</AvatarFallback>
+          </Avatar>
+          <div>
+            <h2 className="text-2xl xs:text-3xl font-bold  text-center lg:text-start">
+              Here is a little background
+            </h2>
+            <p className="text-md xs:text-lg text-center lg:text-justify xl:text-start pt-2 w-full xl:w-[600px] pe-2 leading-[140%]">
+              I am a passionate MERN stack developer with one year of hands-on
+              experience building and deploying dynamic web applications. Having
+              worked on various MERN stack projects, I have gained valuable
+              skills in creating user-friendly and efficient solutions.
+              Currently, I am in my 5th semester of studies, constantly
+              enhancing my knowledge and expertise in web development to deliver
+              innovative and reliable applications.{" "}
+            </p>
+          </div>
         </div>
       </div>
     </>
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -23,7 +23,7 @@ import { setsidebarOpenR } from "@/app/store/Global";
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-const SIDEBAR_WIDTH = "16rem";
+const SIDEBAR_WIDTH = "18rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
 const SIDEBAR_WIDTH_ICON = "3.55rem";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
